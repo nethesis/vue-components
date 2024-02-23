7eee631b857830ac46ac65bfd56c534ce8cdf212
Andrea Leardini <andre8244@gmail.com>
2024-02-23 10:02:25 +0100

    refactor(NeExpandable): rework NeExpandable (#21)

--- stories/NeExpandable.stories.ts
+++ stories/NeExpandable.stories.ts
@@ -1,48 +1,90 @@
-//  Copyright (C) 2023 Nethesis S.r.l.
+//  Copyright (C) 2024 Nethesis S.r.l.
 //  SPDX-License-Identifier: GPL-3.0-or-later
 
-import type { Meta, StoryObj } from '@storybook/vue3'
+import { Meta, StoryObj } from '@storybook/vue3'
 import { NeExpandable } from '../src/main'
 
-const meta = {
+const meta: Meta<typeof NeExpandable> = {
   title: 'Utils/NeExpandable',
   component: NeExpandable,
   tags: ['autodocs'],
-  // default values
   args: {
-    title: 'Show more',
-    expanded: false
+    label: 'Label',
+    isExpanded: false,
+    fullWidth: false
   }
-} satisfies Meta<typeof NeExpandable>
-
+}
 export default meta
 type Story = StoryObj<typeof meta>
 
-const template =
-  '<NeExpandable v-bind="args">\
-    <div class="px-2 py-1">\
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\
-    </div>\
-  </NeExpandable>'
+const defaultTemplate = `<NeExpandable v-bind="args">
+  <div class="pt-4 pl-2 text-gray-700 dark:text-gray-300">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </div>
+</NeExpandable>`
 
-export const Collapsed: Story = {
+export const Default: Story = {
   render: (args) => ({
     components: { NeExpandable },
     setup() {
       return { args }
     },
-    template: template
+    template: defaultTemplate
   }),
   args: {}
 }
 
-export const Expanded: Story = {
+export const FullWidth: Story = {
   render: (args) => ({
     components: { NeExpandable },
     setup() {
       return { args }
     },
-    template: template
+    template: defaultTemplate
   }),
-  args: { expanded: true }
+  args: { fullWidth: true }
+}
+
+const customTriggerTemplate = `<NeExpandable v-bind="args">
+  <template #trigger>
+    <span class="underline text-gray-700 dark:text-gray-300">
+      Click here to show more
+    </span>
+  </template>
+  <div class="pt-4 pl-2 text-gray-700 dark:text-gray-300">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </div>
+</NeExpandable>`
+
+export const CustomTrigger: Story = {
+  render: (args) => ({
+    components: { NeExpandable },
+    setup() {
+      return { args }
+    },
+    template: customTriggerTemplate
+  }),
+  args: {}
+}
+
+const customTriggerFullWidthTemplate = `<NeExpandable v-bind="args">
+  <template #trigger>
+    <div class="p-1 rounded bg-sky-200 dark:bg-sky-800 text-gray-700 dark:text-gray-300">
+      Click here to show more
+    </div>
+  </template>
+  <div class="pt-4 pl-2 text-gray-700 dark:text-gray-300">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </div>
+</NeExpandable>`
+
+export const CustomTriggerFullWidth: Story = {
+  render: (args) => ({
+    components: { NeExpandable },
+    setup() {
+      return { args }
+    },
+    template: customTriggerFullWidthTemplate
+  }),
+  args: { fullWidth: true }
 }
