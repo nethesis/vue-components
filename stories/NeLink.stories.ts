02568f2b6011caae24653803720f0174a41223b2
Andrea Leardini <andre8244@gmail.com>
2024-04-11 17:30:38 +0200

    feat: add invertedTheme prop to NeLink

--- stories/NeLink.stories.ts
+++ stories/NeLink.stories.ts
@@ -8,12 +8,15 @@ const meta: Meta<typeof NeLink> = {
   title: 'Visual/NeLink',
   component: NeLink,
   tags: ['autodocs'],
-  args: {}
+  args: {
+    invertedTheme: false
+  }
 }
 export default meta
 type Story = StoryObj<typeof meta>
 
-const template = '<NeLink v-bind="args" class="text-sm">Go to Wikipedia</NeLink>'
+const template =
+  '<NeLink v-bind="args" href="https://www.wikipedia.org/" target="_blank" class="text-sm">Go to Wikipedia</NeLink>'
 
 export const Default: Story = {
   render: (args) => ({
@@ -24,4 +27,24 @@ export const Default: Story = {
     template: template
   }),
   args: {}
+}
+
+const invertedThemeTemplate = `<div class="text-sm p-4 bg-gray-800 dark:bg-gray-300">
+  <p class="mb-2 text-gray-300 dark:text-gray-800">
+    Inverted theme is useful on inverted backgrounds (e.g. inside a tooltip)
+  </p>
+  <NeLink v-bind="args" href="https://www.wikipedia.org/" target="_blank">
+    Go to Wikipedia
+  </NeLink>
+</div>`
+
+export const InvertedTheme: Story = {
+  render: (args) => ({
+    components: { NeLink },
+    setup() {
+      return { args }
+    },
+    template: invertedThemeTemplate
+  }),
+  args: { invertedTheme: true }
 }
